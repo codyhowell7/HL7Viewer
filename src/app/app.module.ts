30d96f72ad9a1870af572f564669877f491b296e
Aaron Mount <amount@sharedsvs.com>
2017-01-30 11:54:24 -0500

    Checkpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { MessageComponent } from './components/workspace/message/message.component';
+import { OutputComponent } from './components/workspace/output/output.component';
+import { ToolbarComponent } from './components/workspace/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    MenuComponent,
+    WorkspaceComponent,
+    MessageComponent,
+    OutputComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
